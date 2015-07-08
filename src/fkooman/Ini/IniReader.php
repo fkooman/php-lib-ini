f59df711e16815a589eae60a2a41bd0612cf2080
François Kooman <fkooman@tuxed.net>
2015-07-08 23:11:26 +0200

    source formatting

--- src/fkooman/Ini/IniReader.php
+++ src/fkooman/Ini/IniReader.php
@@ -1,20 +1,20 @@
 <?php
 
 /**
-* Copyright 2013 François Kooman <fkooman@tuxed.net>
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright 2015 François Kooman <fkooman@tuxed.net>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 namespace fkooman\Ini;
 
@@ -58,7 +58,7 @@ class IniReader
 
         // find the max depth count, the (optional) required bool and (optional)
         // default value
-        for ($i = 0; $i < count($p); $i++) {
+        for ($i = 0; $i < count($p); ++$i) {
             if (is_bool($p[$i])) {
                 if (0 === $i) {
                     throw new InvalidArgumentException('no configuration field requested');
@@ -66,7 +66,7 @@ class IniReader
                 $maxCount = $i;
                 $required = $p[$i];
                 if ($i < count($p) - 1) {
-                    $default = $p[$i+1];
+                    $default = $p[$i + 1];
                 }
                 // we are done, ignore the other parameters
                 break;
@@ -81,7 +81,7 @@ class IniReader
         $configPointer = $this->config;
 
         // traverse the array until the config value was found
-        for ($i = 0; $i < $maxCount; $i++) {
+        for ($i = 0; $i < $maxCount; ++$i) {
             if (!array_key_exists($p[$i], $configPointer)) {
                 // does not exist
                 if ($required) {
